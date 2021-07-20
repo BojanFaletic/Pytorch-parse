a34f4652633e38428cc0304ed0704df638af4c2f
Bojan Faletic <bojanf.1996@gmail.com>
2021-07-20 04:18:37 +0200

    delete prev files

--- cpp/header/string_util.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stdint.h>
-// find idx of first occurrence of key in string of length c_size
-
-// search from beggining of string to end
-int find_first_of(char const *ch, uint32_t c_size, char const *key);
-
-// search from end of string to beginning
-int find_reverse_first_of(char const *ch, uint32_t c_size, char const *key);
